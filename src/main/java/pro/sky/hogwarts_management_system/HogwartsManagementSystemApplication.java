fa8c477fff1c9ae436c1dfe1c39bb0a76dd90180
freddycooper <almosttanonymous@gmail.com>
2025-02-10 15:18:34 +0300

    initial commit

---
+++ src/main/java/pro/sky/hogwarts_management_system/HogwartsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.hogwarts_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/hogwarts_management_system/HogwartsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hogwarts_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
